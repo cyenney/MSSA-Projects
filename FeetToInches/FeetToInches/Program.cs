124e63e59dc24136070be5b999e0cffcb1e02da5
cyenney <46497569+cyenney@users.noreply.github.com>
2020-05-27 13:28:57 -0400

    Add files via upload

---
+++ FeetToInches/FeetToInches/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeetToInches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the number of feet to be converted to inches.");
+            string inputValueA = Console.ReadLine();
+
+            double feetToinches = double.Parse(inputValueA);
+
+            double totalInches = feetToinches * 12;
+
+            Console.WriteLine(inputValueA + " feet to inches is " + totalInches);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ FindWhichIsGreater/FindWhichIsGreater/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWhichIsGreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter 2 values to find which is greater. one at a time");
+            string inputValue1 = Console.ReadLine();
+            Console.WriteLine("please enter the second value.");
+            string inputValue2 = Console.ReadLine();
+
+            double firstValue = double.Parse(inputValue1);
+            double secondValue = double.Parse(inputValue2);
+
+            if (firstValue > secondValue)
+            {
+                Console.WriteLine(inputValue1 + " is greater than " + inputValue2);
+            }
+
+            else if (firstValue < secondValue)
+            {
+                Console.WriteLine(inputValue2 + " is greater than " + inputValue1);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
